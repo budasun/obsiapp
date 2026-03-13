cf67d5a5a0a31e150423afa19b2428cf6636a7fb
budasun <polaridadual@gmail.com>
2026-03-12 23:30:47 -0600

    Hardcode Firebase credentials for testing

--- services/firebase.ts
+++ services/firebase.ts
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY.trim(),
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN.trim(),
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID.trim(),
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET.trim(),
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID.trim(),
-    appId: import.meta.env.VITE_FIREBASE_APP_ID.trim(),
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID.trim()
+    apiKey: "AIzaSyAKhewBO7qI8imJAOC0GNjxyZXnXKzW_nE",
+    authDomain: "obsiappbase.firebaseapp.com",
+    projectId: "obsiappbase",
+    storageBucket: "obsiappbase.appspot.com",
+    messagingSenderId: "1055119037026",
+    appId: "1:1055119037026:web:24bbc4d1a1a58eab79773b",
+    measurementId: "G-YMNZYEYL93"
 };
 
 console.log("ID del Proyecto detectado:", firebaseConfig.projectId);
